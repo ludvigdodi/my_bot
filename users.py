c160361db9c49aee71b6505f186664bb36fb9b6f
Ludvig Dodi <ludvigdodi.job@gmail.com>
2022-11-17 17:14:48 +0200

    users.py added

---
+++ users.py
@@ -0,0 +1,16 @@
+users = [
+    {"username": "Ludvig", "level": 1},
+    {"username": "Patric", "level": 2},
+    {"username": "Katy", "level": 0},
+    {"username": "Sam", "level": 1},
+]
+
+# user_name = input("Enter your name ")
+# language_level = input(f"Hello {user_name}! \nWhat is your language level?")
+
+# user = {"username": user_name, "level": language_level}
+# users.append(user)
+
+# print(users)
+
+user = {"username": "Ludvig", "level": 1}
